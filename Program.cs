4bba452d45e8709ad1c1a3e9fa0361e0a33ee3fc
Vladimir <vova_spirid@mail.ru>
2023-02-17 22:23:01 +0400

    pull request

--- Program.cs
+++ Program.cs
@@ -23,7 +23,6 @@
   
 // }
 
-
 // Console.Clear();
 // int[] array = new int[5];
 // InputArray(array);
@@ -36,33 +35,33 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-void InputArray(int[] array)
-{
-  for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1, 21); // [1, 20]
-}
+// void InputArray(int[] array)
+// {
+//   for (int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(1, 21); // [1, 20]
+// }
 
-void ReleaseArray (int [] array)
-{
-    for (int i =0; i < array.Length; i++);
-    Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
-}
+// void ReleaseArray (int [] array)
+// {
+//     for (int i =0; i < array.Length; i++);
+//     Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+// }
   
-Console.Clear();
-Console.Write ("Введите число элементов массива: ");
-int a = Convert.ToInt32 (Console.ReadLine());
-int [] numbers = new int [a];
-InputArray (numbers);
-ReleaseArray (numbers);
-int sum = 0;
-for (int i = 0; i < a; i++)
-{
-    if (i % 2 != 0)
-    {
-    sum = sum + numbers[i];
-    }
-}
-Console.WriteLine ("Cумма элементов стоящих на нечетном месте: "+sum);
+// Console.Clear();
+// Console.Write ("Введите число элементов массива: ");
+// int a = Convert.ToInt32 (Console.ReadLine());
+// int [] numbers = new int [a];
+// InputArray (numbers);
+// ReleaseArray (numbers);
+// int sum = 0;
+// for (int i = 0; i < a; i++)
+// {
+//     if (i % 2 != 0)
+//     {
+//     sum = sum + numbers[i];
+//     }
+// }
+// Console.WriteLine ("Cумма элементов стоящих на нечетном месте: "+sum);
 
 
 
@@ -70,7 +69,44 @@ Console.WriteLine ("Cумма элементов стоящих на нечет�
 
 // [3.1, 7.23, 22.57, 2.45, 78.78] -> 76.33
 
+Console.Write ("Введите число элементов массива: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+double [] numbers = new double [a];
+InputArray (numbers);
+ReleaseArray (numbers);
 
+double max = numbers [0];
+for (int i = 0; i < a; i++)
+    {
+        if (numbers[i] > max)
+         {
+            max = numbers[i];
+         }
+    }
+double min = numbers [0];
+for (int i = 0; i < a; i++)
+    {
+        if (numbers[i] < min)
+         {
+            min = numbers[i];
+         }
+    }
+double diff = max - min;
+Console.WriteLine ("Разница между max и min эл. массива: "+diff);
+void InputArray (double [] array)
+  {
+    for (int i =0; i < array.Length; i++)
+    {
+     
+       numbers [i] =  Math.Round(new Random().NextDouble() * 100, 2); //Смена диапазона чисел массива
+      
+    }
+    Console.Clear();
+    Console.WriteLine($"Начальный массив: [{string.Join(", ", array )}]");
+  }
 
-
-
+  void ReleaseArray (double [] array)
+  {
+    for (int i =0; i < array.Length; i++);
+   
+  }
